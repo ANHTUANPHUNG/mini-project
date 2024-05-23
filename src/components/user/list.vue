c892fcf76e924ce0b5cb7652f9665baa82c721ae
ANHTUANPHUNG <anhtuan.phung.16@gmail.com>
2024-05-23 07:03:04 +0700

    m

--- src/components/user/list.vue
+++ src/components/user/list.vue
@@ -14,8 +14,13 @@
           <div class="col-sm-3 col-12">
             <menu-item />
             <div>
-              <span style="font-weight: 900; text-transform: uppercase">Sản phẩm bán chạy</span>
-              <div class="mt-3" style="border-radius: 9px; background-color: #f287ff">
+              <span style="font-weight: 900; text-transform: uppercase"
+                >Sản phẩm bán chạy</span
+              >
+              <div
+                class="mt-3"
+                style="border-radius: 9px; background-color: #f287ff"
+              >
                 <div class="border-bottom menu-item">Đặc sản</div>
                 <div class="border-bottom menu-item">Đồ ăn</div>
                 <div class="border-bottom menu-item">Thức uống</div>
@@ -31,11 +36,13 @@
                   >Đặc sản</span
                 >
               </div>
-              <div class="col-11" style="background-color: greenyellow;" >
-                <div class="row" style=" height: 380px;">
-                  <div class="col-4">a</div>
-                  <div class="col-4">b</div>
-                  <div class="col-4">c</div>
+              <div class="col-11" style="background-color: greenyellow">
+                <div class="carousel-container">
+                  <button @click="prev" class="nav-button">‹</button>
+                  <div class="carousel" ref="carousel">
+                    <div class="item" v-for="(item, index) in displayItems" :key="index">{{ item }}</div>
+                  </div>
+                  <button @click="next" class="nav-button">›</button>
                 </div>
               </div>
               <div class="col-12">
@@ -45,8 +52,13 @@
                   >Đặc sản</span
                 >
               </div>
-              <div class="col-11" >
-                <div class="w-100" style="background-color: greenyellow; height: 380px;">a</div>
+              <div class="col-11">
+                <div
+                  class="w-100"
+                  style="background-color: greenyellow; height: 380px"
+                >
+                  a
+                </div>
               </div>
               <div class="col-12">
                 <span
@@ -55,9 +67,11 @@
                   >Đặc sản</span
                 >
               </div>
-              <div class="col-11" >
-                <div class="row" style="background-color: greenyellow; height: 380px;">
-                </div>
+              <div class="col-11">
+                <div
+                  class="row"
+                  style="background-color: greenyellow; height: 380px"
+                ></div>
               </div>
             </div>
           </div>
@@ -81,10 +95,52 @@ export default {
     Carousel,
     MenuItem,
   },
+  data() {
+    return {
+      items: ["a", "b", "c", "d", "e", "f", "g", "h", "i"],
+      currentIndex: 0,
+    };
+  },
+  computed: {
+    displayItems() {
+      const start = this.currentIndex;
+      const end = start + 3;
+      return this.items.slice(start, end).concat(this.items.slice(0, Math.max(0, end - this.items.length)));
+    },
+  },
+  methods: {
+    next() {
+      this.currentIndex = (this.currentIndex + 1) % this.items.length;
+    },
+    prev() {
+      this.currentIndex = (this.currentIndex - 1 + this.items.length) % this.items.length;
+    },
+  },
   created() {},
 };
 </script>
 <style scoped>
+.carousel-container {
+  display: flex;
+  align-items: center;
+}
+.carousel {
+  display: flex;
+  overflow: hidden;
+  width: 300px; /* Adjust based on your needs */
+}
+.item {
+  min-width: 100px; /* Adjust based on your needs */
+  text-align: center;
+  flex: 0 0 auto;
+}
+.nav-button {
+  background: none;
+  border: none;
+  font-size: 2em;
+  cursor: pointer;
+  user-select: none;
+}
 .user-wrapper {
   margin-top: 90px;
 }
